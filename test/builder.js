5aa2d2727309ddda6f219a39ef254dc37ad55550
Tobias Gurtzick <magic@wizardtales.com>
2014-11-29 19:36:52 +0100

    added test for buildArr

--- test/builder.js
+++ test/builder.js
@@ -20,7 +20,7 @@ lab.experiment( 'builder/', function ()
     lab.experiment( 'subArr', function ()
     {
 
-        lab.test( 'returns true if the element itself, if it is null or unedfined', function ( done )
+        lab.test( 'returns true if the element itself is null or undefined', function ( done )
         {
 
             Code.expect( build.subArr( undefined, [] ) ).equal( undefined );
@@ -45,8 +45,7 @@ lab.experiment( 'builder/', function ()
         lab.test( 'returns true when the processed array was substracted as expected, double layer (only first element counts)', function ( done )
         {
 
-            var
-            finArr = [
+            var finArr = [
                 [ 'test1', 'test2', 'test3' ],
                 [ 'test5', 'test6' ]
             ],
@@ -72,7 +71,7 @@ lab.experiment( 'builder/', function ()
     lab.experiment( 'buildArr', function ()
     {
 
-        lab.test( 'returns true if the element itself, if it is null or unedfined', function ( done )
+        lab.test( 'returns true if one element itself is null or undefined and the other one gets returned', function ( done )
         {
 
             Code.expect( build.buildArr( undefined ) ).equal( undefined );
@@ -101,6 +100,86 @@ lab.experiment( 'builder/', function ()
 
             Code.expect( actual ).to.be.an.array();
             Code.expect( actual ).to.deep.equal( finObj );
+            done();
+        } );
+    } );
+
+    lab.experiment( 'diffArr', function ()
+    {
+
+        lab.test( 'returns true if the element itself is null or undefined', function ( done )
+        {
+
+            var finArr1 = [
+                [],
+                []
+            ],
+                finArr2 = [
+                    [],
+                    []
+                ],
+                diffArr1 = [ 'test1', 'test2' ],
+                diffArr2 = [ 'test3', 'test4' ];
+
+            finArr1[ 0 ] = diffArr1;
+            finArr2[ 1 ] = diffArr2;
+
+
+            var res1 = build.diffArr( diffArr1, undefined );
+            var res2 = build.diffArr( undefined, diffArr2 );
+
+            Code.expect( res1 ).to.be.an.array();
+            Code.expect( res1 ).to.deep.equal( finArr1 );
+
+            Code.expect( res2 ).to.be.an.array();
+            Code.expect( res2 ).to.deep.equal( finArr2 );
+
+            done();
+        } );
+
+        lab.test( 'returns true when the processed array differences are correct, single layer', function ( done )
+        {
+
+            var finArr = [
+                [],
+                [ 'test2', 'test4', 'test5' ]
+            ],
+                diffArr1 = [ 'test1', 'test3' ],
+                diffArr2 = [ 'test1', 'test2', 'test3', 'test4', 'test5' ];
+
+            var actual = build.diffArr( diffArr1, diffArr2 );
+
+            Code.expect( actual ).to.be.an.array();
+            Code.expect( actual ).to.deep.equal( finArr );
+            done();
+        } );
+
+        lab.test( 'returns true when the processed array differences are correct, double layer (only first element)', function ( done )
+        {
+
+            var finArr = [
+                [
+                    [ 'test9', 'test5' ]
+                ],
+                [
+                    [ 'test1', 'test2', 'test3' ],
+                    [ 'test5', 'test6' ]
+                ]
+            ],
+                diffArr1 = [
+                    [ 'test4', 'test1' ],
+                    [ 'test9', 'test5' ]
+                ],
+                diffArr2 = [
+                    [ 'test1', 'test2', 'test3' ],
+                    [ 'test4', 'undefined' ],
+                    [ 'test5', 'test6' ]
+                ];
+
+            var actual = build.diffArr( diffArr1, diffArr2 );
+
+            Code.expect( actual ).to.be.an.array();
+            Code.expect( actual ).to.deep.equal( finArr );
             done();
         } );
     } );
